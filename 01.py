ed38f25e101ff9107fd228aa924b5db3a11e81cd
code_ninja <instructor@codeninja.sg>
2025-08-24 15:19:18 +0800

    Update 01.py
    left reset

--- 01.py
+++ 01.py
@@ -11,6 +11,25 @@ name_label.pack()
 name_entry = tk.Entry(root, width=25)
 name_entry.pack(pady=5)
 
+def say_hello():
+    name = name_entry.get().strip()
+    if name:
+        title_label.config(text=f"Hello, {name}!")
+    else:
+        title_label.config(text="Hello Tkinter!" )
+
+hello_btn = tk.Button(root, text="Say Hello",command =say_hello)
+hello_btn.pack(pady=5)
+clicks = tk.IntVar(value=0)
+def count_click():
+    clicks.set(clicks.get()+1)
+    counter_label.config(text=f"Clicks: {clicks.get()}")
+click_btn = tk.Button(root, text="+1 click", command=count_click)
+click_btn.pack(pady=5)
+counter_label = tk.Label(root, text="Clicks=0")
+counter_label.pack()
+
+
+
 
 root.mainloop()
-
