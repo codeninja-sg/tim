6ebe65c5a40a96b734d8f909cd94153345688561
code_ninja <instructor@codeninja.sg>
2025-09-07 15:36:48 +0800

    Create mini-clicker.py

---
+++ mini-clicker.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+root = tk.Tk()
+root.title("Clicker v1")
+
+title = tk.Label(root, text="Baby Clicker",font=("Arial", 18))
+title.grid(row=0, column=0,columnspan=2,pady=10)
+score = tk.IntVar(value=0)
+score_label=tk.Label(root, text="Score: 0", font=("Arial",14))
+score_label.grid(row=1, column=0, columnspan=2, pady=5)
+
+def add_click():
+    score.set(score.get()+1)
+    score_label.config(text=f"Score: {score.get()}")
+
+click_btn = tk.Button(root, text="+1", width = 12, height=2, command= add_click)
+click_btn.grid(row=2, column=0, pady=8, padx=5)
+root.mainloop()
