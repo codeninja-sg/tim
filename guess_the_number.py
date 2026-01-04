104a206dee64437aba7b4ed507420f25a9317ad6
code_ninja <instructor@codeninja.sg>
2026-01-04 15:42:08 +0800

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,5 +1,4 @@
 import tkinter as tk
-import random
 
 root = tk.Tk()
 root.title("Guess the Number")
@@ -12,7 +11,11 @@ title_label.pack(pady=10)
 prompt_label = tk.Label(root, text="Enter a number (1-100):")
 prompt_label.pack()
 
+guess_entry = tk.Entry(root, width=10, justify="center")
+guess_entry.pack(pady=5)
 
+result_label = tk.Label(root, text="Good luck!", font=("Arial", 14))
+result_label.pack(pady=10)
 
 
 
