e20eea1f7957e3b13134af3ee26dfb0a8e0bc7d9
code_ninja <instructor@codeninja.sg>
2026-01-11 15:36:45 +0800

    Update guess_the_number.py

--- guess_the_number.py
+++ guess_the_number.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import random
+
 
 root = tk.Tk()
 root.title("Guess the Number")
@@ -17,7 +19,20 @@ guess_entry.pack(pady=5)
 result_label = tk.Label(root, text="Good luck!", font=("Arial", 14))
 result_label.pack(pady=10)
 
+secret = random.randint(1, 100)
 
+
+def check_guess():
+    text = guess_entry.get().strip()
+
+    if not text.isdigit():
+        result_label.config(text="Please enter a number!")
+        return
+    
+    guess = int(text)
+
+    if guess < secret:
+       result_label.config(text="Too low!")
 
 
 
