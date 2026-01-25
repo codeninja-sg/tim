29e441cb59c5094c3f96f7f4d24bb29dbbed3478
code_ninja <instructor@codeninja.sg>
2026-01-25 15:33:08 +0800

    Create drawingpad.py

---
+++ drawingpad.py
@@ -0,0 +1,25 @@
+import tkinter as tk
+
+root = Tk.tk
+root.title("Drawing Pad")
+root.geometry("800x600")
+
+
+controls = tk.Frame(root)
+controls.grid(row=0, column=0, sticky="ew, padx=8, pady=8")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+root.mainloop()
