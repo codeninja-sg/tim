f3a4d3d728c2e6496873890e7ef4803ee0f44d74
code_ninja <instructor@codeninja.sg>
2026-02-01 15:29:38 +0800

    Update drawingpad.py

--- drawingpad.py
+++ drawingpad.py
@@ -1,12 +1,25 @@
 import tkinter as tk
 
-root = Tk.tk
+root = tk.Tk()
 root.title("Drawing Pad")
 root.geometry("800x600")
 
 
 controls = tk.Frame(root)
-controls.grid(row=0, column=0, sticky="ew, padx=8, pady=8")
+controls.grid(row=0, column=0, sticky="ew", padx=8, pady=8)
+
+
+canvas = tk.Canvas(root, bg="white",width=760,height=480)
+canvas.grid(row=1, column=0,sticky="nsew",padx=8,pady=8)
+
+root.grid_rowconfigure(1, weight=1)
+root.grid_columnconfigure(0, weight=1)
+
+
+
+
+
+root.mainloop()
 
 
 
